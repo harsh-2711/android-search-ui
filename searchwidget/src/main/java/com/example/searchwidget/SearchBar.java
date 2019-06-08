2a6fb14a716852650cae11deeabf418b54d455ef
Harsh Patel <harshaudi27@gmail.com>
2019-06-09 03:18:17 +0530

    Added Suggestions Adapter and OnSearchActionListner interface

--- searchwidget/src/main/java/com/example/searchwidget/SearchBar.java
+++ searchwidget/src/main/java/com/example/searchwidget/SearchBar.java
@@ -6,10 +6,83 @@ import android.content.res.TypedArray;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.PopupMenu;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.searchwidget.adapter.SuggestionsAdapter;
 
 public class SearchBar extends RelativeLayout {
+
+    public static final int BUTTON_SPEECH = 1;
+    public static final int BUTTON_NAVIGATION = 2;
+    public static final int BUTTON_BACK = 3;
+    public static final int VIEW_VISIBLE = 1;
+    public static final int VIEW_INVISIBLE = 0;
+    private CardView searchBarCardView;
+    private LinearLayout inputContainer;
+    private ImageView navIcon;
+    private ImageView menuIcon;
+    private ImageView searchIcon;
+    private ImageView arrowIcon;
+    private ImageView clearIcon;
+    private EditText searchEdit;
+    private TextView placeHolder;
+    private View suggestionDivider;
+    private View menuDivider;
+    private OnSearchActionListener onSearchActionListener;
+    private boolean searchEnabled;
+    private boolean suggestionsVisible;
+    private boolean isSuggestionsEnabled = true;
+    private SuggestionsAdapter adapter;
+    private float destiny;
+
+    private PopupMenu popupMenu;
+
+    private int navIconResId;
+    private int menuIconRes;
+    private int searchIconRes;
+    private int speechIconRes;
+    private int arrowIconRes;
+    private int clearIconRes;
+
+    private boolean speechMode;
+    private int maxSuggestionCount;
+    private boolean navButtonEnabled;
+    private boolean roundedSearchBarEnabled;
+    private boolean menuDividerEnabled;
+    private int dividerColor;
+    private int searchBarColor;
+
+    private CharSequence hintText;
+    private CharSequence placeholderText;
+    private int textColor;
+    private int hintColor;
+    private int placeholderColor;
+    private int navIconTint;
+    private int menuIconTint;
+    private int searchIconTint;
+    private int arrowIconTint;
+    private int clearIconTint;
+
+    private boolean navIconTintEnabled;
+    private boolean menuIconTintEnabled;
+    private boolean searchIconTintEnabled;
+    private boolean arrowIconTintEnabled;
+    private boolean clearIconTintEnabled;
+    private boolean borderlessRippleEnabled = false;
+
+    private int textCursorColor;
+    private int highlightedTextColor;
+
+    //Nav/Back Arrow Flag
+    private boolean navIconShown = true;
 
     public SearchBar(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
@@ -32,5 +105,31 @@ public class SearchBar extends RelativeLayout {
 
         TypedArray array = getContext().obtainStyledAttributes(attributeSet, R.styleable.SearchBar);
         array.recycle();
+    }
+
+    /**
+     * Interface definition for MaterialSearchBar callbacks.
+     */
+    public interface OnSearchActionListener {
+        /**
+         * Invoked when SearchBar opened or closed
+         *
+         * @param enabled state
+         */
+        void onSearchStateChanged(boolean enabled);
+
+        /**
+         * Invoked when search confirmed and "search" button is clicked on the soft keyboard
+         *
+         * @param text search input
+         */
+        void onSearchConfirmed(CharSequence text);
+
+        /**
+         * Invoked when "speech" or "navigation" buttons clicked.
+         *
+         * @param buttonCode {@link #BUTTON_NAVIGATION}, {@link #BUTTON_SPEECH} or {@link #BUTTON_BACK} will be passed
+         */
+        void onButtonClicked(int buttonCode);
     }
 }
