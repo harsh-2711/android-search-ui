a759b419cd119d0b2860a576d129dcba180c70bc
Harsh Patel <harshaudi27@gmail.com>
2019-06-11 20:01:44 +0530

    Initialised Search Bar in main app

--- app/src/main/java/com/example/searchwidgetdemo/MainActivity.java
+++ app/src/main/java/com/example/searchwidgetdemo/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.searchwidgetdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.searchwidget.SearchBar;
+
 public class MainActivity extends AppCompatActivity {
+
+    SearchBar searchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchBar = (SearchBar) findViewById(R.id.searchBar);
     }
 }
