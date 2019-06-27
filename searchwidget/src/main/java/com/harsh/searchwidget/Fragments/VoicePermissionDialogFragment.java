d29b5150287cb346363d6485a24a22f3c2f009c1
Harsh Patel <harshaudi27@gmail.com>
2019-06-28 04:01:23 +0530

    Added voice permissions dialog

---
+++ searchwidget/src/main/java/com/harsh/searchwidget/Fragments/VoicePermissionDialogFragment.java
@@ -0,0 +1,32 @@
+package com.harsh.searchwidget.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.harsh.searchwidget.R;
+
+public class VoicePermissionDialogFragment extends DialogFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.VoiceDialogTheme);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.voice_permission_dialog, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
