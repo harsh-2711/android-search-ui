b434a0d02dfcd36f6ab7521238f92a9ac17026b5
Harsh Patel <harshaudi27@gmail.com>
2019-06-28 04:43:45 +0530

    Completed VoicePermissionDialog fragment

--- searchwidget/src/main/java/com/harsh/searchwidget/Fragments/VoicePermissionDialogFragment.java
+++ searchwidget/src/main/java/com/harsh/searchwidget/Fragments/VoicePermissionDialogFragment.java
@@ -1,9 +1,13 @@
 package com.harsh.searchwidget.Fragments;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatImageView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +15,11 @@ import android.view.ViewGroup;
 import com.harsh.searchwidget.R;
 
 public class VoicePermissionDialogFragment extends DialogFragment {
+
+    private final int PERMISSIONS_RECORD_AUDIO = 1;
+
+    AppCompatImageView cancel;
+    AppCompatButton positive, negative;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,6 +36,32 @@ public class VoicePermissionDialogFragment extends DialogFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        cancel = (AppCompatImageView) view.findViewById(R.id.cancel);
+        positive = (AppCompatButton) view.findViewById(R.id.positive);
+        negative = (AppCompatButton) view.findViewById(R.id.negative);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        positive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.RECORD_AUDIO},
+                        PERMISSIONS_RECORD_AUDIO);
+            }
+        });
+
+        negative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
     }
 
 }
