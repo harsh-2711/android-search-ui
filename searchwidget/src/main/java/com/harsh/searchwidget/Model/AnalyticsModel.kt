9652e328296ea92b792fdd7957f37d5093fcbf8b
Ashish Magadum <31703946+ShonenAsh@users.noreply.github.com>
2020-10-11 12:50:20 +0530

    Update AnalyticsModel.kt

--- searchwidget/src/main/java/com/harsh/searchwidget/Model/AnalyticsModel.kt
+++ searchwidget/src/main/java/com/harsh/searchwidget/Model/AnalyticsModel.kt
@@ -1,5 +1,9 @@
 package com.harsh.searchwidget.Model
 
+/**
+ * A Kotlin Data class that holds properties of Searches
+ */
+
 data class AnalyticsModel(
         var xSearchId: String?,
         var isXSearchClick: Boolean,
